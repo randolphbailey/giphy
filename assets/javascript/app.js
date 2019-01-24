db4a7fc713e137ebae97455d792393e0ba4628b2
Randolph Bailey <Randolph Bailey>
2019-01-23 19:16:14 -0600

    add favorites functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 var topics = ["Funny Cats", "Birds", "Funny Dogs", "Giraffes", "Explosions"];
+var favorites = [];
 
 //Ajax Construction
 function fetchGifs () {
@@ -16,7 +17,8 @@ function populateGifs (r) {
             $("#main-content").append("<div class='row' id='row"+j+"'><div class='card-deck' id='deck" + j+ "'></div>");
         }
         console.log(r.data[i].images.original_still.url);
-        let card = "<div class='card m-2'><img src='" + r.data[i].images.original.url + "' id='" + r.data[i].id + "' class='card-img-top' alt-src='" + r.data[i].images.original_still.url + "'><div class='card-body'><h5 class='card-title'>" + r.data[i].title + "</h5><p class='card-text'>Rating: " + r.data[i].rating + "</p></div></div>";
+        let card = "<div class='card m-2'><img src='" + r.data[i].images.original.url + "' const='" + r.data[i].images.original.url + "' id='" + r.data[i].id + "' class='card-img-top play-pause' alt-src='" + r.data[i].images.original_still.url + "' smallfixed='" + r.data[i].images.fixed_height_small.url + "'><div class='card-body'><h5 class='card-title'>" + r.data[i].title + "</h5><p class='card-text'>Rating: " + r.data[i].rating + "</p></div><div class='card-footer'><a class='btn btn-outline-warning btn-sm add-favorite' parentid='" + r.data[i].id + "'><i class='far fa-star' parentid='" + r.data[i].id + "'></i></a></div></div>";
+        //console.log(card);
         $("#deck" + j).append(card);
     }
 }
@@ -37,12 +39,39 @@ function addTopic () {
     initTopics();
 }
 
+//Add gif to favorites dropup
+function addFavorite (event) {
+    let parentID = $(event.target).attr("parentid");
+    let constURL = $("#" + parentID).attr("const");
+    let smallGif = $("#" + parentID).attr("smallfixed");
+    let newFav = {original: constURL, small: smallGif};
+    favorites.push(newFav);
+    initFavorites();
+}
+
+//Initialize Favorites List
+function initFavorites () {
+    $("#favoritesList").empty();
+    for (let i=0; i<favorites.length; i++) {
+        $("#favoritesList").append("<img class='dropdown-item fav-gif' src='" + favorites[i].small + "' biggif='" + favorites[i].original + "'>");
+    }
+    $("#favoritesBadge").text(favorites.length);
+}
+
 //Play and/or pause gifs on click
 function playPause(event) {
-    let clicked = $("#" + event.target.id);
+    let clicked = $(event.target);
     let newSource = clicked.attr("alt-src");
     let oldSource = clicked.attr("src");
     clicked.attr("alt-src", oldSource).attr("src", newSource);
+}
+
+//Popup for favorite gifs
+function showModal(event) {
+    let clicked = $(event.target);
+    let modalSrc = clicked.attr("biggif");
+    $("#modal-img").attr("src", modalSrc);
+    $("#favoritesModal").modal("show");
 }
 
 //EVENT HANDLERS
@@ -50,5 +79,7 @@ $(function() {
     initTopics();
     $(document).on("click", ".gif-link", function() { $("li, a").removeClass("active"); $(this).addClass("active"); fetchGifs(); });
     $(document).on("click", "#addTopicButton", addTopic);
-    $(document).on("click", ".card-img-top", playPause);
+    $(document).on("click", ".play-pause", playPause);
+    $(document).on("click", ".add-favorite", addFavorite);
+    $(document).on("click", ".fav-gif", showModal);
 });
